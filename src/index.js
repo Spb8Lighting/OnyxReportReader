fcfa598f39783a5708016328a52e1055ce869497
Spb8Lighting <sylvain.guiblain@gmail.com>
2019-01-17 00:46:42 +0100

    Reload Fixture Group at index loading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import Input from './input'
 import DB from './database'
 import PatchRender from './render/patch'
+import GroupRender from './render/group'
 
 // Change the form label by their associated picture
 Input()
 
+// Load Patch if this last exists
 DB.Get({ Object: 'File', ItemID: 'Patch' })
   .then(item => {
     if (typeof item !== 'undefined') {
       PatchRender()
     }
+    // Load Fixture Group if Patch has been already done
+    DB.Get({ Object: 'File', ItemID: 'FixtureGroup' })
+      .then(item => {
+        if (typeof item !== 'undefined') {
+          GroupRender()
+        }
+      })
   })
 
+// Reset Link
 document.querySelector('a[href="#Reset"]').addEventListener('click', e => {
   e.preventDefault()
   DB.DeleteDB('ReportReader')
