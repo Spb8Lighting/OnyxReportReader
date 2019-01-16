418ae9902f8d24c13a0c2cb77d9c9715ecd07348
Spb8Lighting <sylvain.guiblain@gmail.com>
2019-01-17 00:45:53 +0100

    Integrate Fixture Group in the Parser

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,6 @@
 const $Show = require('./object/show')
 const $Fixture = require('./object/fixture')
+const $FixtureGroup = require('./object/fixturegroup')
 const DB = require('./database')
 
 module.exports = {
@@ -16,6 +17,19 @@ module.exports = {
         DB.Add({
           Object: 'Fixture',
           Item: JSON.parse(JSON.stringify(new $Fixture($Fixtures[i])))
+        })
+      }
+    }
+  },
+  FixtureGroup: $Xml => {
+    // Set XML Getter
+    let $General = $Xml.getElementsByTagName('FixtureGroups')[0]
+    let $Group = $General.getElementsByTagName('FixtureGroup')
+    for (let i = 0; i < $Group.length; i++) {
+      if ($Group.hasOwnProperty(i)) {
+        DB.Add({
+          Object: 'FixtureGroup',
+          Item: JSON.parse(JSON.stringify(new $FixtureGroup($Group[i])))
         })
       }
     }
