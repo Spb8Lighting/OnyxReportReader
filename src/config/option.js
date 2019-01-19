4559cb56c1b800c927b0e57154960edaa67a13c4
Spb8Lighting <sylvain.guiblain@gmail.com>
2019-01-19 01:56:01 +0100

    feat(display): Display Fixture list in the group page #9

--- src/config/option.js
+++ src/config/option.js
@@ -1,5 +1,9 @@
 module.exports = {
   Patch: {
     DisplayInvertAxesIcon: false
+  },
+  Group: {
+    // In the group page, permit to simplified the Fixture abbreviation title to the minimum
+    DisplaySimplifiedFixture: true
   }
 }
--- src/render/group.js
+++ src/render/group.js
@@ -1,39 +1,26 @@
 'use strict'
 const DB = require('./../database')
 const Display = require('./../display')
+const Option = require('./../config/option')
 
 const NotFalse = val => {
   return val === false ? '' : val
 }
 
-let SetGroupTable = async (Content, Groups) => {
-  for (let i = 0; i < Object.keys(Groups).length; ++i) {
-    let Group = Groups[i]
-    let Fixtures = 0
-    let FixtureList = []
-    if (Group.Fixtures) {
-      Fixtures = Object.keys(Group.Fixtures).length
+let FixtureInfo = FixtureDB => {
+  if (Option.Group.DisplaySimplifiedFixture) {
+    if (FixtureDB.Name) {
+      return `${FixtureDB.Manufacturer} - ${FixtureDB.Model}(${FixtureDB.Name}) @ ${FixtureDB.Universe}.${FixtureDB.Address}`
+    } else {
+      return `${FixtureDB.Manufacturer} - ${FixtureDB.Model} @ ${FixtureDB.Universe}.${FixtureDB.Address}`
     }
-    let GetAllFixtureId = async () => {
-      if (Fixtures > 0) {
-        for (let i = 0; i < Fixtures; ++i) {
-          let Fixture = Group.Fixtures[i]
-          let FixtureDB = await DB.Get({ Object: 'Fixture', Index: 'Ref', ItemID: Fixture })
-          if (FixtureDB) {
-            FixtureList.push(FixtureDB.ID)
-          }
-        }
-      }
+  } else {
+    if (FixtureDB.Name) {
+      return `Name: ${FixtureDB.Name},Manufacturer: ${FixtureDB.Manufacturer}, Model: ${FixtureDB.Model}, Address: ${FixtureDB.Universe}.${FixtureDB.Address}`
+    } else {
+      return `Manufacturer: ${FixtureDB.Manufacturer}, Model: ${FixtureDB.Model}, Address: ${FixtureDB.Universe}.${FixtureDB.Address}`
     }
-    await GetAllFixtureId()
-    Content.tbody.push('<tr>' + '\n' +
-      '\t' + '<td class="number">' + Group.ID + '</td>' + '\n' +
-      '\t' + '<td>' + NotFalse(Group.Name) + '</td>' + '\n' +
-      '\t' + '<td>' + ((Group.Mask) ? 'True' : '') + '</a></td>' + '\n' +
-      '\t' + '<td>' + FixtureList.join(', ') + '</td>' + '\n' +
-      '</tr>')
   }
-  return Content
 }
 
 let Render = () => {
@@ -50,25 +37,45 @@ let Render = () => {
     DB.GetAll({ Object: 'FixtureGroup' })
       .then(Groups => {
         DB.Get({ Object: 'Show', ItemID: 'Show' })
-          .then(Show => {
-            SetGroupTable(Content, Groups)
-              .then(Content => {
-                Content.Description = 'Fixture groups summary: ' + Show.Name
-                Content.Table = '<table class="fixturegroup">' + '\n' +
-                  '<thead>' + '\n' +
-                  Content.thead + '\n' +
-                  '</thead>' + '\n' +
-                  '<tbody>' + '\n' +
-                  Content.tbody.join('\n') + '\n' +
-                  '</tbody>' + '\n' +
-                  '</table>'
+          .then(async Show => {
+            for (let i = 0; i < Object.keys(Groups).length; ++i) {
+              let Group = Groups[i]
+              let Fixtures = 0
+              let FixtureList = []
+              if (Group.Fixtures) {
+                Fixtures = Object.keys(Group.Fixtures).length
+              }
+              if (Fixtures > 0) {
+                for (let i = 0; i < Fixtures; ++i) {
+                  let Fixture = Group.Fixtures[i]
+                  let FixtureDB = await DB.Get({ Object: 'Fixture', Index: 'Ref', ItemID: Fixture })
+                  if (FixtureDB) {
+                    FixtureList.push(`<abbr title="${FixtureInfo(FixtureDB)}">${FixtureDB.ID}</abbr>`)
+                  }
+                }
+              }
+              Content.tbody.push('<tr>' + '\n' +
+                '\t' + '<td class="number">' + Group.ID + '</td>' + '\n' +
+                '\t' + '<td>' + NotFalse(Group.Name) + '</td>' + '\n' +
+                '\t' + '<td>' + ((Group.Mask) ? 'True' : '') + '</a></td>' + '\n' +
+                '\t' + '<td class="number">' + FixtureList.join(', ') + '</td>' + '\n' +
+                '</tr>')
+            }
+            Content.Description = 'Fixture groups summary: ' + Show.Name
+            Content.Table = '<table class="fixturegroup">' + '\n' +
+              '<thead>' + '\n' +
+              Content.thead + '\n' +
+              '</thead>' + '\n' +
+              '<tbody>' + '\n' +
+              Content.tbody.join('\n') + '\n' +
+              '</tbody>' + '\n' +
+              '</table>'
 
-                let FixtureGroupArticle = document.getElementById('FixtureGroup')
+            let FixtureGroupArticle = document.getElementById('FixtureGroup')
 
-                FixtureGroupArticle.innerHTML = `<h2>Fixture Groups</h2><p>${Content.Description}</p>${Content.Table}`
-                Display.SetLoaded('FixtureGroup')
-                resolve(true)
-              })
+            FixtureGroupArticle.innerHTML = `<h2>Fixture Groups</h2><p>${Content.Description}</p>${Content.Table}`
+            Display.SetLoaded('FixtureGroup')
+            resolve(true)
           })
       })
   })
