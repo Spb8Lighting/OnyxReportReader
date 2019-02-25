3a0c7cb226228567b34a65fdb71fd4e44f883b42
Spb8Lighting <sylvain.guiblain@gmail.com>
2019-02-25 14:55:48 +0100

    update(console): Split console layout to have independant playback part

--- src/function/physical.js
+++ src/function/physical.js
@@ -51,8 +51,9 @@ export const Console = {
       let M1HDH2 = document.createElement('h2')
       M1HDH2.innerHTML = `M1 HD <em>(${NumberOfBanks} bank${NumberOfBanks > 1 ? 's' : ''})</em>`
       M1HD.appendChild(M1HDH2)
+      const SVGM1HD = await Dexie.waitFor(GetConsole('M1HD'))
       for (let z = 0; z < NumberOfBanks; z++) {
-        let NewM1HD = await Dexie.waitFor(GetConsole('M1HD'))
+        let NewM1HD = SVGM1HD.cloneNode(true)
         // NewM1HD.querySelector('.Bank tspan').innerHTML = TriDigit(z + 1)
         NewM1HD.querySelector('style').remove()
 
@@ -95,8 +96,15 @@ export const Console = {
       let MTouchH2 = document.createElement('h2')
       MTouchH2.innerHTML = `M-Touch <em>(${NumberOfBanks} bank${NumberOfBanks > 1 ? 's' : ''})</em>`
       MTouch.appendChild(MTouchH2)
+
+      let Wrapper = document.createElement('div')
+      Wrapper.className = 'flex-container'
+      let LeftSide = document.createElement('div')
+      LeftSide.className = 'LeftSide'
+
+      const SVGMTouch = await Dexie.waitFor(GetConsole('MTouch'))
       for (let z = 0; z < NumberOfBanks; z++) {
-        let NewMTouch = await Dexie.waitFor(GetConsole('MTouch'))
+        let NewMTouch = SVGMTouch.cloneNode(true)
         NewMTouch.querySelector('.Bank tspan').innerHTML = TriDigit(z + 1)
         NewMTouch.querySelector('style').remove()
 
@@ -112,31 +120,63 @@ export const Console = {
             }
           }
         }
-        MTouch.appendChild(NewMTouch.documentElement)
+        LeftSide.appendChild(NewMTouch.documentElement)
       }
+      Wrapper.appendChild(LeftSide)
+      MTouch.appendChild(Wrapper)
       return MTouch
     })
   },
   MPlay: async () => {
     return Db.transaction('r', Db.Cuelist, Db.Physical, async () => {
+      // Get number of bank info per type
       let Prefix = 'SubmasterPlayback'
       let ListOfBanks = await Db.Physical.where('TypePageBank').startsWith(Prefix).uniqueKeys()
       let NumberOfBanks = ListOfBanks.length
+
+      // Create the console container
       let MPlay = document.createElement('div')
       MPlay.className = 'M-Play'
       let MPlayH2 = document.createElement('h2')
       MPlayH2.innerHTML = `M-Play <em>(${NumberOfBanks} bank${NumberOfBanks > 1 ? 's' : ''})</em>`
       MPlay.appendChild(MPlayH2)
-      for (let z = 0; z < NumberOfBanks; z += 2) {
-        let NewMPlay = await Dexie.waitFor(GetConsole('MPlay'))
-        NewMPlay.querySelector('.Bank tspan').innerHTML = TriDigit(z + 1)
-        NewMPlay.querySelector('.BankSub tspan').innerHTML = TriDigit(z + 2)
-        NewMPlay.querySelector('style').remove()
+
+      let Wrapper = document.createElement('div')
+      Wrapper.className = 'flex-container'
+      let LeftSide = document.createElement('div')
+      LeftSide.className = 'LeftSide'
+      let RightSide = document.createElement('div')
+      RightSide.className = 'RightSide'
+
+      // Get the console SVG image
+      const SVGMPlay = await Dexie.waitFor(GetConsole('MPlay'))
+      SVGMPlay.querySelector('style').remove()
+
+      // Create a dedicated SVG picture for the left part
+      let MPlayLeft = SVGMPlay.cloneNode(true)
+      MPlayLeft.querySelector('svg').setAttribute('viewBox', '0 0 465 304.5')
+      MPlayLeft.querySelector('g.rightside').remove()
+
+      // Create a dedicated SVG picture for the right part
+      let MplayRight = SVGMPlay.cloneNode(true)
+      MplayRight.querySelector('svg').setAttribute('viewBox', '465 0 147.5 304.5')
+      MplayRight.querySelector('g.leftside').remove()
+
+      // Create all parts following the number of banks
+      for (let z = 0; z < NumberOfBanks; z++) {
+        // Clone SVG picture the current bank
+        let CurrentMPlayLeft = MPlayLeft.cloneNode(true)
+        let CurrentMplayRight = MplayRight.cloneNode(true)
+        // Set the Bank Number
+        CurrentMPlayLeft.querySelector('.Bank tspan').innerHTML = TriDigit(z + 1)
+        CurrentMplayRight.querySelector('.Bank tspan').innerHTML = TriDigit(z + 1)
+
+        // Loop other number of playbacks for the left side
         for (let i = 1; i <= 24; i++) {
           let CurrentFader = await DbGet({ Object: 'Physical', Index: 'TypePageBankPosition', ItemID: `${ListOfBanks[z]}-${i}` })
           if (CurrentFader) {
             let Cuelist = await DbGet({ Object: 'Cuelist', Index: 'ID', ItemID: CurrentFader.CuelistID })
-            CurrentFader = NewMPlay.querySelector(`.SubFader${i}`)
+            CurrentFader = CurrentMPlayLeft.querySelector(`.SubFader${i}`)
             if (typeof Cuelist === 'object') {
               CurrentFader.classList.add(`Type-${Cuelist.Type}`)
               CurrentFader.innerHTML = `<a href="#Cuelist-${Cuelist.ID}">${CurrentFader.innerHTML}</a>`
@@ -144,11 +184,12 @@ export const Console = {
             }
           }
         }
+        // Loop other number of playbacks for the right side
         for (let i = 1; i <= 24; i++) {
-          let CurrentFader = await DbGet({ Object: 'Physical', Index: 'TypePageBankPosition', ItemID: `${ListOfBanks[z + 1]}-${i}` })
+          let CurrentFader = await DbGet({ Object: 'Physical', Index: 'TypePageBankPosition', ItemID: `${ListOfBanks[z]}-${i}` })
           if (CurrentFader) {
             let Cuelist = await DbGet({ Object: 'Cuelist', Index: 'ID', ItemID: CurrentFader.CuelistID })
-            CurrentFader = NewMPlay.querySelector(`.Button${i}`)
+            CurrentFader = CurrentMplayRight.querySelector(`.Button${i}`)
             if (typeof Cuelist === 'object') {
               CurrentFader.classList.add(`Type-${Cuelist.Type}`)
               CurrentFader.innerHTML = `<a href="#Cuelist-${Cuelist.ID}">${CurrentFader.innerHTML}</a>`
@@ -156,8 +197,12 @@ export const Console = {
             }
           }
         }
-        MPlay.appendChild(NewMPlay.documentElement)
+        LeftSide.appendChild(CurrentMPlayLeft.documentElement)
+        RightSide.appendChild(CurrentMplayRight.documentElement)
       }
+      Wrapper.appendChild(LeftSide)
+      Wrapper.appendChild(RightSide)
+      MPlay.appendChild(Wrapper)
       return MPlay
     })
   }
